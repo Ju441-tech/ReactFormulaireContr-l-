0441248201fed37dea8623e31b98bb69276c366a
Julien Roche <merlin441977@gmail.com>
2023-11-25 17:29:13 +0100

    React input contôlé

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 
 import { useState } from 'react'
 
-//Le formulaire est dit contrôlé parceque parcequ'il appelle à chaque saisie la fonction handle change puis fait un render aussi donc à chaque saisie
+//Le formulaire est dit contrôlé. Ici, à chaque fois qu'on saisit une valeur dans le input, à chaquelettre (ou donnée), 
+//on appelle la fonction handleChange parcequ'elle écoute tous les changements à chaque entrée dans input, pour n'envoyer la saisit qu'une fois terminée, il faut faire un submit sur form, pas de fonction sur input
 function App() {
 
   //useState simple changement de valeur unique d'une variable :
